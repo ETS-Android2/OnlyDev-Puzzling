d06ab42c9d49abd392407e85da951ca58bcb58b5
Shaila Gibert <sgibertma@uoc.edu>
2022-03-31 19:23:33 +0100

    Shaila's commit: pause/settings/play UI and activities creation: - finishing play_menu.xml - Creating activities    - Pause    - PlayPuzzle    - Settings

---
+++ app/src/main/java/com/example/myfirstapp/PlayPuzzle.java
@@ -0,0 +1,63 @@
+package com.example.myfirstapp;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class PlayPuzzle extends AppCompatActivity {
+    ArrayList<Bitmap> pieces;
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_menu); //Matching this class with the play_menu UI
+
+        final ConstraintLayout layout = findViewById(R.id.playMenuUI);
+        ImageView puzzle = findViewById(R.id.puzzle);
+
+        // run image related code after the view was laid out
+        // to have all dimensions calculated
+        puzzle.post(new Runnable(){
+            @Override
+            public void run(){
+                pieces = splitImage();
+                for (Bitmap piece : pieces){
+                    ImageView puzzlePieces = new ImageView(getApplicationContext());
+                    puzzlePieces.setImageBitmap(piece);
+                    layout.addView(puzzlePieces);
+                }
+            }
+        });
+    }
+
+    private ArrayList<Bitmap> splitImage() {
+        int piecesAmount = 12;
+        int rows = 4;
+        int columns = 4;
+
+        ImageView imageView = findViewById(R.id.puzzle);
+        ArrayList<Bitmap> pieces = new ArrayList<>(piecesAmount);
+        //Get Bitmap from source image aka puzzle
+        BitmapDrawable puzzleDrawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = puzzleDrawable.getBitmap();
+        //Get width and height for the pieces that will be made
+        int widthFromPieces = bitmap.getWidth() / columns; //In order to fulfill the amount set for the columns
+        int heightFromPieces = bitmap.getHeight() / rows;
+        //Create the pieces from the ImageView and append them to the arraylist
+        int height = 0;
+        for (int row = 0; row < rows; row++) {
+            int width = 0;
+            for (int column = 0; column < columns; column++) {
+                pieces.add(Bitmap.createBitmap(bitmap, width, height, widthFromPieces, heightFromPieces));
+                width += widthFromPieces;
+            }
+            height += heightFromPieces;
+        }
+        return pieces;
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/Settings.java
@@ -0,0 +1,12 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
+    }
+}
