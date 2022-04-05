94f8b688624f348cb0126a7f74d4702cf96c59f3
lcantalpiedraa <95313233+lcantalapiedraa@users.noreply.github.com>
2022-04-05 21:48:42 +0200

    05/04/2022 21:50

--- app/src/main/java/com/example/myfirstapp/Menu.java
+++ app/src/main/java/com/example/myfirstapp/Menu.java
@@ -4,16 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        Button playBtn=findViewById(R.id.playButton);
+        Button settingsBtn=findViewById(R.id.settingsButton);
+        Button exitBtn=findViewById(R.id.exitButton);
+
+        playBtn.setOnClickListener((View.OnClickListener) this);
+        settingsBtn.setOnClickListener((View.OnClickListener) this);
+        exitBtn.setOnClickListener((View.OnClickListener) this);
     }
 
-    public void exit(){
-        System.exit(0);
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.playButton:
+                Intent intent = new Intent(this, Menu.class);
+                startActivity(intent);
+                break;
+
+
+            case R.id.settingsButton: //go to help View
+                intent = new Intent(this, Help.class);
+                startActivity(intent);
+                break;
+
+            case R.id.exitButton: //sale de la aplicacion
+                finish();
+                break;
+        }
     }
 }
