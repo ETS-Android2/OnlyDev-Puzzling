28c804c78956e4b11cefe32e41728311f6448f4f
ShailaGibert <86909131+ShailaGibert@users.noreply.github.com>
2022-04-13 22:58:45 +0100

    Fixing the crashes from the Create and Read methods from SQLManager.createOneUser & SQLManager.retrieveUser.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.myfirstapp;
 
-//import android.content.Intent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-
 import logicClasses.SQLManager;
 import logicClasses.User;
 import logicClasses.UserFactory;
@@ -25,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     // ----- ATTRIBUTES ------------ //
     SQLManager sqlManager = new SQLManager(MainActivity.this, "PuzzlingDatabase",
             null, 1);
+
+    // ---- POSSIBLE INTENTS ------- //
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,98 +43,93 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onClick(View view) {
-                String mail = MainActivity.this.mail.getText().toString();
-                String password = MainActivity.this.mail.getText().toString();
-                //TODO: handler exceptions if needed. Test app with possible errors
-                //Instantiate object if there are valid values inside the input fields
-                if (mail.isEmpty() || password.isEmpty() || !(mail.contains("@"))){
-                    Toast.makeText(MainActivity.this,
-                            "Please, enter valid mail and password.",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    User user = UserFactory.createUser(mail, password);
-                    //TODO: Check if user already exists [NOT WORKING]
-                    ArrayList<String> userRetrieved = MainActivity.this.sqlManager.retrieveUser(user.getMail());
-                    if (userRetrieved.isEmpty()){
-                        try{ //Add user to database
+                if (MainActivity.this.areFieldsFulfill()) {
+                    User user = UserFactory.createUser(MainActivity.this.mail.getText().toString(),
+                            MainActivity.this.password.getText().toString());
+
+                    User userRetrieved = MainActivity.this.sqlManager.retrieveUser(user.getMail());
+                    if (userRetrieved.getMail().isEmpty()) { //There is no user with same account
+                        try { //Add user to database
                             long createStatement = MainActivity.this.sqlManager.createOneUser(user);
                             Toast.makeText(MainActivity.this, "User \"" + user.getMail() +
                                     "\" created successfully.", Toast.LENGTH_LONG).show();
-                            if (createStatement == -1){ //Catch return from createOne
-                                throw new Exception("An error occur adding the user" + user.toString() +
+                            //Getting in Menu layout
+                            Intent startLayout = new Intent(getApplicationContext(), Menu.class);
+                            startActivity(startLayout);
+                            if (createStatement == -1) { //Catch return from createOne
+                                throw new Exception("An error occur adding the user" + user +
                                         "\nPlease try again.");
                             }
-                        } catch (Exception exception){
+                        } catch (Exception exception) {
                             Toast.makeText(MainActivity.this, exception.toString(),
                                     Toast.LENGTH_LONG).show();
                         }
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, "This user already exists.",
-                            Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "This user already exists." +
+                                userRetrieved, Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
 
         this.signIn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You've pressed the sign in button",Toast.LENGTH_SHORT).show();
+                if (MainActivity.this.areFieldsFulfill()) {
+                    String mail = MainActivity.this.mail.getText().toString();
+                    String password = MainActivity.this.password.getText().toString();
+                    User userInserted = new User(mail, password);
+                    User userRetrieved = MainActivity.this.sqlManager.
+                            retrieveUser(mail);
+                    if (userInserted.equals(userRetrieved)) {
+                        //Getting in Menu layout
+                        Intent startLayout = new Intent(getApplicationContext(), Menu.class);
+                        startActivity(startLayout);
+                    } else {
+                        Toast.makeText(MainActivity.this, "The user or password given " +
+                                "is not valid. Please try again.", Toast.LENGTH_LONG).show();
+                    }
+                }
             }
+
         });
 
         this.help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You've pressed the help button",Toast.LENGTH_SHORT).show();
+                Intent helpLayout = new Intent(getApplicationContext(), Help.class);
+                startActivity(helpLayout);
             }
         });
 
         this.exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You've pressed the exit button",Toast.LENGTH_SHORT).show();
+                //Close connection to database
+                MainActivity.this.sqlManager.close();
+                finish();
             }
         });
     }
 
+    public boolean areFieldsFulfill() {
+        String mail = MainActivity.this.mail.getText().toString();
+        String password = MainActivity.this.password.getText().toString();
+        //TODO: handler exceptions if needed. Test app with possible errors
+        //Instantiate object if there are valid values inside the input fields
+        if (mail.isEmpty() || password.isEmpty() || !(mail.contains("@"))) {
+            Toast.makeText(MainActivity.this,
+                    "Please, enter valid mail and password.",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void onClick(View view) {
-//
-//        switch (view.getId()){
-//
-//            case R.id.signIn:
-//                Intent intent = new Intent(this, Menu.class);
-//                startActivity(intent);
-//                break;
-//
-//
-//            case R.id.help: //go to help View
-//                intent = new Intent(this, Help.class);
-//                startActivity(intent);
-//                break;
-//
-//                case R.id.exit: //sale de la aplicacion
-//                finish();
-//                break;
-    }
 
-
-    /*public void playMenu(View view){
-        Intent i = new Intent(this, Menu.class);
-        startActivity(i);
     }
+}
 
-    public void helpPage(View view){
-        Intent i = new Intent(this,Help.class);
-        startActivity(i);
-    }
-
-    public void clickExit(View view){
-        moveTaskToBack(true);
-        android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(1);
-    }*/
-    }
-//}
